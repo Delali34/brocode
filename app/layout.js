c8f621d339ab40103198706576f7e94d263eb098
Dela <ernestanyomitse752@gmail.com>
2023-09-12 22:33:53 +0000

    page

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "The Bro Code Gh",
-  description: "Youtube channel",
+  description:
+    "The Bro Code is not just a platform for men to express themselves; it's a beacon of hope. It's an endeavor to find solutions to the struggles that men face. For far too long, the mental health and well-being of men",
   logo: "../public/WhatsApp Image 2023-08-15 at 11.31.25 PM.jpeg",
 };
 
